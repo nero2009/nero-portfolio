fad55bd5475efddca1731cd0a4ec02b1e0194b35
nero adaware <nero2009@rocketmail.com>
2019-03-13 12:11:05 +0100

    Add Progress illustration ��

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Progress from '../../assets/progress.svg'
 
 const about = () => {
   return (
     <Layout>
-      <div className="layout-container">about</div>
+      <div className="layout-container" style={{ textAlign: 'center' }}>
+        <div>
+          <Progress style={{ width: '70%' }} />
+        </div>
+        <p style={{ marginTop: -30, fontSize: 20, color: '#3f3d56' }}>
+          Work in progress
+        </p>
+      </div>
     </Layout>
   )
 }
